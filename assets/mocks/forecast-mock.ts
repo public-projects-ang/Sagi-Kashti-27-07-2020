9053fb34c33d0b53c2080f2719204b85640221f7
Sagi Kashti <projectk771@gmail.com>
2020-07-30 13:12:33 +0300

    Auto-generated commit

---
+++ assets/mocks/forecast-mock.ts
@@ -0,0 +1,170 @@
+export const forecastRes = {
+  Headline: {
+    EffectiveDate: '2020-08-01T08:00:00+03:00',
+    EffectiveEpochDate: 1596258000,
+    Severity: 4,
+    Text: 'Pleasant this weekend',
+    Category: 'mild',
+    EndDate: null,
+    EndEpochDate: null,
+    MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/extended-weather-forecast/213225?unit=c&lang=en-us',
+    Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?unit=c&lang=en-us'
+  },
+  DailyForecasts: [
+    {
+      Date: '2020-07-30T07:00:00+03:00',
+      EpochDate: 1596081600,
+      Temperature: {
+        Minimum: {
+          Value: 20.8,
+          Unit: 'C',
+          UnitType: 17
+        },
+        Maximum: {
+          Value: 33.3,
+          Unit: 'C',
+          UnitType: 17
+        }
+      },
+      Day: {
+        Icon: 1,
+        IconPhrase: 'Sunny',
+        HasPrecipitation: false
+      },
+      Night: {
+        Icon: 33,
+        IconPhrase: 'Clear',
+        HasPrecipitation: false
+      },
+      Sources: [
+        'AccuWeather'
+      ],
+      MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=1&unit=c&lang=en-us',
+      Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=1&unit=c&lang=en-us'
+    },
+    {
+      Date: '2020-07-31T07:00:00+03:00',
+      EpochDate: 1596168000,
+      Temperature: {
+        Minimum: {
+          Value: 20.6,
+          Unit: 'C',
+          UnitType: 17
+        },
+        Maximum: {
+          Value: 31.1,
+          Unit: 'C',
+          UnitType: 17
+        }
+      },
+      Day: {
+        Icon: 1,
+        IconPhrase: 'Sunny',
+        HasPrecipitation: false
+      },
+      Night: {
+        Icon: 33,
+        IconPhrase: 'Clear',
+        HasPrecipitation: false
+      },
+      Sources: [
+        'AccuWeather'
+      ],
+      MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=2&unit=c&lang=en-us',
+      Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=2&unit=c&lang=en-us'
+    },
+    {
+      Date: '2020-08-01T07:00:00+03:00',
+      EpochDate: 1596254400,
+      Temperature: {
+        Minimum: {
+          Value: 19.2,
+          Unit: 'C',
+          UnitType: 17
+        },
+        Maximum: {
+          Value: 30.4,
+          Unit: 'C',
+          UnitType: 17
+        }
+      },
+      Day: {
+        Icon: 1,
+        IconPhrase: 'Sunny',
+        HasPrecipitation: false
+      },
+      Night: {
+        Icon: 33,
+        IconPhrase: 'Clear',
+        HasPrecipitation: false
+      },
+      Sources: [
+        'AccuWeather'
+      ],
+      MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=3&unit=c&lang=en-us',
+      Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=3&unit=c&lang=en-us'
+    },
+    {
+      Date: '2020-08-02T07:00:00+03:00',
+      EpochDate: 1596340800,
+      Temperature: {
+        Minimum: {
+          Value: 21.4,
+          Unit: 'C',
+          UnitType: 17
+        },
+        Maximum: {
+          Value: 30.5,
+          Unit: 'C',
+          UnitType: 17
+        }
+      },
+      Day: {
+        Icon: 1,
+        IconPhrase: 'Sunny',
+        HasPrecipitation: false
+      },
+      Night: {
+        Icon: 33,
+        IconPhrase: 'Clear',
+        HasPrecipitation: false
+      },
+      Sources: [
+        'AccuWeather'
+      ],
+      MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=4&unit=c&lang=en-us',
+      Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=4&unit=c&lang=en-us'
+    },
+    {
+      Date: '2020-08-03T07:00:00+03:00',
+      EpochDate: 1596427200,
+      Temperature: {
+        Minimum: {
+          Value: 19.5,
+          Unit: 'C',
+          UnitType: 17
+        },
+        Maximum: {
+          Value: 29.6,
+          Unit: 'C',
+          UnitType: 17
+        }
+      },
+      Day: {
+        Icon: 1,
+        IconPhrase: 'Sunny',
+        HasPrecipitation: false
+      },
+      Night: {
+        Icon: 34,
+        IconPhrase: 'Mostly clear',
+        HasPrecipitation: false
+      },
+      Sources: [
+        'AccuWeather'
+      ],
+      MobileLink: 'http://m.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=5&unit=c&lang=en-us',
+      Link: 'http://www.accuweather.com/en/il/jerusalem/213225/daily-weather-forecast/213225?day=5&unit=c&lang=en-us'
+    }
+  ]
+};
