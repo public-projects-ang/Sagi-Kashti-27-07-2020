3493db4e947d79112ca24ba42359afe5cf525f43
Sagi Kashti <projectk771@gmail.com>
2020-08-02 17:33:58 +0300

    compodoc added

---
+++ documentation/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"routes","filename":"src/app/app-routing.module.ts","module":"AppRoutingModule","children":[{"path":"home","component":"HomeComponent"},{"path":"favorites","component":"FavoritesComponent"},{"path":"**","redirectTo":"home"}],"kind":"module"}]}
